0cd83e5442e82a90ab1204964ec7037705ef5ec6
hapidjus <5553121+hapidjus@users.noreply.github.com>
2024-08-28 14:04:56 +0200

    Fix title

--- resources/views/station.blade.php
+++ resources/views/station.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title',  "{$station->availableBikes} - {$station->name}")
+@section('title',  "{$station->available_bikes} cyklar lediga - {$station->name}")
 @section('content')
 <div class="flex flex-col items-center">
     <x-station-card-small :station="$station"/>
